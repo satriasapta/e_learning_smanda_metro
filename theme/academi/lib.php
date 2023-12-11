4679be00a9a61ded8f81fb42f00aa25f21e44873
msdzilii <83410300+msdzilii@users.noreply.github.com>
2023-12-11 16:04:25 +0700

    apus yg kemaren gak jadi

--- theme/academi/lib.php
+++ theme/academi/lib.php
@@ -293,28 +293,3 @@ function theme_academi_get_extra_scss($theme) {
     // Call the parent themes get_extra_scss function.
     return theme_boost_get_extra_scss($theme);
 }
-
-// class theme_academi_transmuted_custom_menu_item extends custom_menu_item {
-//     public function __construct(custom_menu_item $menunode) {
-//         parent::__construct($menunode->get_text(), $menunode->get_url(), $menunode->get_title(), $menunode->get_sort_order(), $menunode->get_parent());
-//         $this->children = $menunode->get_children();
-
-//         $matches = array();
-//         if (preg_match('/^\[\[([a-zA-Z0-9\-\_\:]+)\]\]$/', $this->text, $matches)) {
-//             try {
-//                 $this->text = get_string($matches[1], 'theme_academi');
-//             } catch (Exception $e) {
-//                 $this->text = $matches[1];
-//             }
-//         }
-
-//         $matches = array();
-//         if (preg_match('/^\[\[([a-zA-Z0-9\-\_\:]+)\]\]$/', $this->title, $matches)) {
-//             try {
-//                 $this->title = get_string($matches[1], 'theme_academi');
-//             } catch (Exception $e) {
-//                 $this->title = $matches[1];
-//             }
-//         }
-//     }
-// }
