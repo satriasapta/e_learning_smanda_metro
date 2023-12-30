00b8b48e3b1d767d3979c8e9a549fdf036656151
msdzilii <83410300+msdzilii@users.noreply.github.com>
2023-12-30 20:14:49 +0700

    jadiin grafik chart guru

--- my/lib.php
+++ my/lib.php
@@ -108,77 +108,92 @@ function chartsiswa()
 
 function chartGuru()
 {
-    global $USER, $DB, $OUTPUT;
+    global $OUTPUT, $DB, $USER;
 
     $userid = $USER->id;
     $isteacher = user_has_role_assignment($userid, 3);
+
     if (!$isteacher) {
-        echo "Anda bukan guru.";
+        echo "Anda Bukan Guru";
         exit;
     }
 
-    // Mengecek apakah ada kursus yang dipilih.
+    // Cek kursus yang dipilih.
     $selected_courseid = !empty($_POST['courseid']) ? $_POST['courseid'] : null;
 
     // Mengambil kursus yang diampu oleh guru.
     $sql = "SELECT c.id, c.fullname FROM {course} c
             JOIN {context} ctx ON ctx.instanceid = c.id
             JOIN {role_assignments} ra ON ra.contextid = ctx.id
-            WHERE ctx.contextlevel = 50 AND ra.roleid = 3 AND ra.userid = :userid";  // roleid 3 untuk guru
+            WHERE ctx.contextlevel = 50 AND ra.roleid = 3 AND ra.userid = :userid";
     $params = ['userid' => $userid];
     $courses = $DB->get_records_sql($sql, $params);
 
     // Membuat dropdown.
     echo '<form method="post">';
-    echo '<select name="courseid">';
+    echo '<select name="courseid" style="margin-right: 25px;">';
     foreach ($courses as $course) {
         $isSelected = ($course->id == $selected_courseid) ? 'selected' : '';
         echo '<option value="' . $course->id . '" ' . $isSelected . '>' . $course->fullname . '</option>';
     }
     echo '</select>';
-    echo '<input type="submit" value="Tampilkan Chart"/>';
+    echo '<input type="submit" value="Tampilkan Chart" style="margin-bottom: 45px;"/>';
     echo '</form>';
 
+    // Jika kursus telah dipilih, jalankan query untuk nilai dan buat chart.
     if ($selected_courseid) {
-        // Query untuk mengambil nama kuis dan nilai siswa dari tabel mdl_quiz.
-        $sql = "SELECT qz.name AS quizname, gg.finalgrade
-                FROM {quiz} qz
-                JOIN {course_modules} cm ON cm.instance = qz.id
-                JOIN {modules} m ON m.id = cm.module
-                JOIN {grade_items} gi ON gi.iteminstance = cm.instance
+        // Query untuk mengambil nilai kuis, grade to pass, dan jumlah siswa.
+        $sql = "SELECT gi.itemname, gg.finalgrade, gi.gradepass, COUNT(DISTINCT gg.userid) AS studentcount
+                FROM {grade_items} gi
                 JOIN {grade_grades} gg ON gi.id = gg.itemid
-                WHERE gi.courseid = :courseid
-                AND gi.itemmodule = 'quiz'
-                AND gg.userid = :userid
-                AND m.name = 'quiz'
-                ORDER BY qz.name";
+                JOIN {course} c ON gi.courseid = c.id
+                WHERE gi.courseid = :courseid AND gi.itemmodule = 'quiz' AND gi.itemtype = 'mod'
+                GROUP BY gi.itemname, gg.finalgrade, gi.gradepass
+                ORDER BY gi.itemname";
 
-        $params = ['courseid' => $selected_courseid, 'userid' => $userid];
-        $quiz_grades = $DB->get_records_sql($sql, $params);
+        $params = ['courseid' => $selected_courseid];
+        $grades_data = $DB->get_records_sql($sql, $params);
 
-        // Cek jika ada kuis dan hasil kuis yang ditemukan
-        if (empty($quiz_grades)) {
-            echo "Tidak ada kuis atau hasil kuis yang ditemukan untuk kursus ini.";
+        if (empty($grades_data)) {
+            echo '<div style="margin-bottom: 25px;">Tidak ada nilai kuis yang ditemukan untuk kursus ini.</div>';
         } else {
+            // Mengolah data untuk chart.
             $quiz_names = [];
-            $student_grades = [];
+            $quiz_grades = [];
+            $grades_to_pass = [];
+            $student_counts = [];
 
-            foreach ($quiz_grades as $quiz_grade) {
-                $quiz_names[] = $quiz_grade->quizname;
-                $student_grades[] = (float) $quiz_grade->finalgrade;
+            foreach ($grades_data as $data) {
+                $quiz_name = $data->itemname . ' (' . $data->studentcount . ' students)'; // Menambahkan jumlah siswa ke label
+                $quiz_names[] = $quiz_name;
+                $quiz_grades[] = (float) $data->finalgrade;
+                $grades_to_pass[] = (float) $data->gradepass;
+                $student_counts[] = $data->studentcount;
             }
 
-            // Membuat chart bar untuk nilai kuis.
-            $grades_series = new \core\chart_series('Quiz Grades', $student_grades);
-            $chart = new \core\chart_bar(); // Menggunakan bar chart untuk representasi nilai
-            $chart->set_title('Student Quiz Grades Chart');
+            // Membuat series untuk chart.
+            $grades_series = new \core\chart_series('Quiz Grades', $quiz_grades);
+            $pass_series = new \core\chart_series('Grades to Pass', $grades_to_pass);
+
+            // Membuat chart bar.
+            $chart = new \core\chart_bar();
+            $chart->set_title('Quiz Grades Chart');
             $chart->add_series($grades_series);
+            $chart->add_series($pass_series);
             $chart->set_labels($quiz_names);
+
+            // Menampilkan informasi jumlah siswa di setiap label
+            foreach ($quiz_names as $index => $name) {
+                $student_count = $student_counts[$index];
+                $quiz_names[$index] = "{$name} ({$student_count} students)";
+            }
 
             // Menampilkan chart.
             echo $OUTPUT->render($chart);
         }
     }
+
+
 }
 function my_get_page(?int $userid, int $private = MY_PAGE_PRIVATE, string $pagename = MY_PAGE_DEFAULT)
 {
